e03910f486c3cd520b08a41eb16a417060212db9
sugarvoid <gamedev@kitargo.com>
2025-01-17 16:14:59 -0600

    Recolor gameover images

--- src/main.lua
+++ src/main.lua
@@ -37,6 +37,19 @@ g_state=nil
 
 deliveries_left = 4 --10
 
+end_text_l1=""
+end_text_l2=""
+
+endings={
+    "local mailman goes missing",
+    "local mailman turns to red splat",
+    "local mailman missing",
+    "[demon attacked]",
+    "dog claims mailman's life",
+    "mailman fired",
+    "mailman quits, buys city",
+}
+
 function update_objects()
     for o in all(objects.front) do
         o:update()
@@ -434,4 +447,25 @@ function goto_bonus()
     spawner.reset()
     bouns_timer=30*30
     change_state(gamestates.bonus)
+end
+
+
+
+function draw_gameover()
+    cls(7)
+    spr(192,32,8,8,2)
+    print("$0.25",6,10,5)
+    rect(4,30,124,120,5)
+    pal(14,0)
+    spr(end_spr[1],80,34,4,4)
+    pal()
+    print("game over",game_over_x,1,0)
+    print(end_text_l1,10,40,0)
+end
+
+function update_gameover()
+    game_over_x+=1
+    if game_over_x==130 then
+        game_over_x=-30
+    end
 end
